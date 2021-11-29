ebdbe0ea341dee3554cc3b8eaeaa71d66743220a
Ajay <ajaybains2004@gmail.com>
2021-11-29 22:44:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useRef } from "react";
 function App() {
+  const [randomInput, setRandomInput] = useState("");
+  const [seconds, setSeconds] = useState(0);
+  const renders = useRef(0);
+  const inputRef = useRef();
+  const timerID = useRef();
+
+  const handleChange = (e) => {
+    setRandomInput(e.target.value);
+    renders.current++;
+  };
+
+  // const focusOnInput = () => {
+  //   inputRef.current.focus();
+  // };
+
+  const startTimer = () => {
+    timerID.current = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+      renders.current++;
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    clearInterval(timerID.current);
+    timerID.current = 0;
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    if (seconds) {
+      renders.current++;
+      setSeconds(0);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="app">
+      <input
+        ref={inputRef}
+        type="text"
+        value={randomInput}
+        placeholder="Random Input"
+        onChange={handleChange}
+      />
+      <p>Renders :{renders.current}</p>
+      <br />
+      <br />
+      <section>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </section>
+      <br />
+      <br />
+      <p>{seconds}</p>
+      <br />
+      <br />
+      <p>{randomInput}</p>
+    </main>
   );
 }
 
